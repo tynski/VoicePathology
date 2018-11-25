5e229df352e6ae0cc62cfda816179039c38efc5d
tynski <b.tynski@gmail.com>
2018-11-25 17:31:14 +0100

    code fixes

--- PythonCode/getparameters.py
+++ PythonCode/getparameters.py
@@ -1,35 +1,47 @@
-import os 
+"""
+Alghoritm iterates for every audio file in given directory.
+Each audio file is processed in order to extract parameters.
+Finally all paremters vectors for each signal are saved to CSV file.
+"""
+
+import os
 from scipy.io import wavfile
 import numpy as np
 import pandas as pd
 
-def parameterstocsv(datadirectory, patientstatus):
-	df2Csv = input('Enter file name:')
-	df2Csv += '.csv'
-	filepath = os.path.join('/home/barti/Documents/voicePathology/voiceDataset/', datadirectory)
-	files = os.listdir(filepath)
-	columns = ['STD','MEAN','MAX','MIN','RMS','ENERGY','POWER']
-	cases = pd.DataFrame(columns = columns)
-	for filename in files:
-		data = returnparameters(filepath, filename)
-		data = np.reshape(data, (1,len(columns)))
-		df = pd.DataFrame(data = data, columns = columns)
-		cases = cases.append(df)
-	cases['Status'] = patientstatus
-	cases.to_csv(df2Csv, index = False)
 
-def returnparameters(filepath, filename):
-	audio = wavfile.read(os.path.join(filepath, filename))[1]
-	audio = audio[~np.isnan(audio)]
-	return voiceparameters(audio.tolist())
+def parameterstocsv(datadir, patientstatus):
+    """
+    Parameters
+    ----------
+    datadir
+        Audio files directory
+    patientstatus
+        Healthy or pathology
+    """
+    df2Csv = input('Enter file name to save CSV:') + '.csv'
+    allfiles = os.listdir(datadir)
+    columns = ['STD', 'MEAN', 'MAX', 'MIN', 'RMS', 'ENERGY']
+    cases = pd.DataFrame(columns=columns)
+    for filename in allfiles:
+        filepath = os.path.join(datadir, filename)
+        cases.loc[len(cases)] = returnparametersdf(filepath)
+    cases['Status'] = patientstatus
+    cases.to_csv(df2Csv, index=False)
 
-def voiceparameters(signal):
-	N = len(signal)
-	STD = np.std(signal)
-	MEAN = np.mean(signal)
-	MAX = np.amax(signal)
-	MIN = np.amin(signal)
-	RMS = np.sqrt(np.mean(np.square(signal)))
-	ENERGY = np.sum(np.power(signal, 2))
-	POWER = np.sum(np.power(signal, 2)/(2*N + 1))
-	return [STD,MEAN,MAX,MIN,RMS,ENERGY,POWER]
+
+def returnparametersdf(filepath):
+    signal = wavfile.read(filepath)[1]
+    if(~(np.isnan(np.sum(signal)))):
+        parametersvect = parameters(signal.tolist())
+        return parametersvect
+
+
+def parameters(signal):
+    STD = np.std(signal)
+    MEAN = np.mean(signal)
+    MAX = np.amax(signal)
+    MIN = np.amin(signal)
+    RMS = np.sqrt(np.mean(np.square(signal)))
+    ENERGY = np.sum(np.power(signal, 2))
+    return np.array([STD, MEAN, MAX, MIN, RMS, ENERGY])
