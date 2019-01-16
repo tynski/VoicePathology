0b222061c5618c3cbb192b6dbbc28d4c529be7cb
tynski <b.tynski@gmail.com>
2019-01-16 15:47:42 +0100

    final ver

--- PythonCode/extract_2.py
+++ PythonCode/extract_2.py
@@ -1,8 +1,10 @@
 """
 Alghoritm iterates for every audio file in given directory.
+File directory have to be absolute.
 Each audio file is processed in order to extract parameters.
 Finally all paremters vectors for each y are saved to CSV file.
 """
+
 import os
 import numpy as np
 import pandas as pd
@@ -14,12 +16,16 @@ from scipy.signal import blackmanharris
 
 def parameterstocsv(datadir, patientstatus):
     """
-    Parameters
+    Attributes
     ----------
     datadir
-        Audio files directory
+        Absolute audio files directory
     patientstatus
         Healthy or pathology
+
+    Methods
+    ----------
+    Returns csv file with extracted features.
     """
     fulldir = os.path.join(os.getcwd(), datadir)
     df2Csv = input('Enter file name to save CSV:') + '.csv'
@@ -42,31 +48,37 @@ def parameterstocsv(datadir, patientstatus):
                'KURTOSIS',
                'SKEWNESS']
     cases = pd.DataFrame(columns=columns)
+
     for filename in allfiles:
         filepath = os.path.join(datadir, filename)
         cases.loc[len(cases)] = parameters(filepath)
+
     cases['Status'] = patientstatus
     cases.to_csv(df2Csv, index=False)
 
 
 def parameters(filepath):
+    """
+    Compute parametrs vector for each audio file.
+    """
     y, sr = librosa.load(filepath)
-    #N = len(y)
+
     if(~(np.isnan(np.sum(y)))):
         framelength = 512
         n_mfcc = 10
         parameters = []
+
         mfcc = librosa.feature.mfcc(y=y,
                                     sr=sr,
                                     hop_length=framelength,
                                     n_mfcc=n_mfcc)
+
         for i in range(n_mfcc):
             parameters.append(np.mean(mfcc[i]))
-        RMS = librosa.feature.rmse(y,
-                                   hop_length=framelength)
+
+        RMS = librosa.feature.rmse(y, hop_length=framelength)
         parameters.append(np.mean(RMS))
-        ZCR = librosa.feature.zero_crossing_rate(y,
-                                                 hop_length=framelength)
+        ZCR = librosa.feature.zero_crossing_rate(y, hop_length=framelength)
         parameters.append(np.mean(ZCR))
         F0 = fundamental_freq(y, sr, framelength)
         parameters.append(F0)
@@ -78,10 +90,14 @@ def parameters(filepath):
         parameters.append(KURTOSIS)
         SKEWNESS = skew(y)
         parameters.append(SKEWNESS)
+
         return parameters
 
 
 def fundamental_freq(y, sr, framelength):
+    """
+    Find fundametal freq with use of FFT.
+    """
     N = len(y)
     f0 = []
     for i in range(framelength, N, framelength):
@@ -89,6 +105,7 @@ def fundamental_freq(y, sr, framelength):
         end = i
         frame = y[start:end]
         f0.append(freq_from_fft(frame, sr))
+
     return np.mean(f0)
 
 
@@ -96,12 +113,12 @@ def freq_from_fft(sig, fs):
     """
     Estimate frequency from peak of FFT
     """
-    # Compute Fourier transform of windowed y
+
     windowed = sig * blackmanharris(len(sig))
     f = np.fft.fft(windowed)
 
     # Find the peak and interpolate to get a more accurate peak
-    i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
+    i = np.argmax(abs(f))
     true_i = parabolic(np.log(abs(f)), i)[0]
 
     # Convert to equivalent frequency
@@ -109,24 +126,21 @@ def freq_from_fft(sig, fs):
 
 
 def parabolic(f, x):
-    """Quadratic interpolation for estimating the true position of an
+    """
+    Quadratic interpolation for estimating the true position of an
     inter-sample maximum when nearby samples are known.
- 
-    f is a vector and x is an index for that vector.
-   
+
+    Parameters
+    -----------
+    f is a vector 
+    x is an index for that vector.
+
+    Method
+    -----------
     Returns (vx, vy), the coordinates of the vertex of a parabola that goes
     through point x and its two neighbors.
-   
-    Example:
-    Defining a vector f with a local maximum at index 3 (= 6), find local
-    maximum if points 2, 3, and 4 actually defined a parabola.
-   
-    In [3]: f = [2, 3, 1, 6, 4, 2, 3, 1]
-   
-    In [4]: parabolic(f, argmax(f))
-    Out[4]: (3.2142857142857144, 6.1607142857142856)
-   
     """
     xv = 1 / 2. * (f[x - 1] - f[x + 1]) / (f[x - 1] - 2 * f[x] + f[x + 1]) + x
     yv = f[x] - 1 / 4. * (f[x - 1] - f[x + 1]) * (xv - x)
+
     return (xv, yv)
