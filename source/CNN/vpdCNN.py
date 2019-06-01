95f8b12eb164461aa707e1c9c78120bd52585170
tynski <b.tynski@gmail.com>
2019-06-01 19:29:22 +0200

    CNN new arc

--- source/CNN/vpdCNN.py
+++ source/CNN/vpdCNN.py
@@ -56,10 +56,14 @@ train_data.shape
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Dropout(0.25))
 
 model.summary()
 
@@ -68,6 +72,7 @@ model.summary()
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(2, activation='sigmoid'))
 
 model.summary()
