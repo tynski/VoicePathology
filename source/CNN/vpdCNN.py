70cd8d359ed72e17a5ac93da83f378c78a3ab03f
tynski <b.tynski@gmail.com>
2019-06-04 21:21:56 +0000

    2 CNN before pooling

--- source/CNN/vpdCNN.py
+++ source/CNN/vpdCNN.py
@@ -55,10 +55,12 @@ train_data.shape
 # # Convolutional base
 
 model = models.Sequential()
-model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+model.add(layers.Conv2D(8, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+model.add(layers.Conv2D(8, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 
-model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+model.add(layers.Conv2D(16, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 
 #model.add(layers.Conv2D(64, (3,3), activation='relu'))
@@ -70,19 +72,21 @@ model.summary()
 # # Dense layer at the top
 
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(2, activation='sigmoid'))
 model.summary()
 
 
 # # Train model
 adamax = optimizers.Adamax(lr=0.005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+adagrad = optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
+rmsprop = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 
 model.compile(optimizer=adamax,
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(train_data, train_labels, epochs=40, validation_data=(val_data,val_labels))
+history = model.fit(train_data, train_labels, epochs=30, validation_data=(val_data,val_labels))
 
 
 # # Test model
